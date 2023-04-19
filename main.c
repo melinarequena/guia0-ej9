68ffeead325b6a4bff37bf07bbcd6d04cdf6db50
Melina <mrequena@fue.undef.edu.ar>
2023-04-19 15:49:31 -0300

    Ej 9:)

--- main.c
+++ main.c
@@ -1,6 +1,23 @@
-#include <stdio.h>
+/* Guia 0, Ej 9
+ 9. Defina una función que calcule la longitud de una cadena de caracteres. */
 
-int main() {
-    printf("Hello, World!\n");
+#include<stdio.h>
+void calcular(char *);
+
+int main(){
+    char texto[1000], *textoptr;
+    textoptr =  &texto;
+    printf("Ingrese un texto:\n");
+    gets(texto);
+    calcular(textoptr);
     return 0;
+}
+
+void calcular(char *textoptr){
+    int i, cont=0;
+    for(i=0; textoptr[i]!='\0'; i++)
+    {
+       cont++;
+    }
+    printf("La longitud de la cadena es de %d", cont);
 }
